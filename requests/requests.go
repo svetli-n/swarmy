4188e4080b7cd2eb5af3e50801aa9df2689d4bb5
Svetlin Stefanov <svetlin.stefanov@findwise.com>
2016-12-21 22:58:03 +0100

    Refact.

---
+++ requests/requests.go
@@ -0,0 +1,21 @@
+package requests
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+)
+
+func Post(name, url, data string) {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(data)))
+	if err != nil {
+		log.Fatal("Request error: ", err)
+	}
+	req.Header.Add("If-None-Match", `W/"wyzzy"`)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	_ = resp
+	if err != nil {
+		log.Fatal("Request error: ", err)
+	}
+}
